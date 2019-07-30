f845fc2e3bafdf16f4d3185124378db5f631b6c4
Matthew Mendero <matthewmendero@gmail.com>
2019-07-29 23:11:15 -0400

    Command Handler Created

--- bot.js
+++ bot.js
@@ -16,69 +16,34 @@ fs.readdir(`./commands/`, (err, files) => {
         return;
     }
 
-    console.log(`Loading ${jsfiles.length}" commands!`); 
+    console.log(`Loading ${jsfiles.length} commands!`); 
     jsfiles.forEach((f,i) => {
         let props = require(`./commands/${f}`);
         console.log(`${i + 1}: ${f} loaded!`);
-        bot.commands.set(f,props);
+        bot.commands.set(props.help.name, props);
     });
 });
 
 bot.on('ready', () =>{
-    console.log('HypeJester is Now Online!');
+    console.log('Darvis is Now Online!');
     console.log(bot.commands);
     bot.user.setActivity('the servers dumb commands', {type: 'LISTENING'}).catch(console.error);
 });
 
-bot.on
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
 
-// bot.on('message', msg=>{
-//     let args = msg.content.substring(PREFIX.length).split(" ");
+    let messageArray = message.content.split(/\s+/g);
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
 
-//     switch(args[0]){
-//         case 'info':
-//             if(args[1] === 'version'){
-//                 msg.channel.sendMessage('Version: '+version);
-//             }
-//             else if(args[1] === 'commands'){
-//                 msg.channel.sendMessage('Current Emote Options:\n!emote sad\n!emote pog\n!emote lmao\n!emote frick\n!emote YES');
-//             }
-//             else{
-//                 msg.channel.sendMessage('!version --> Current Bot Version\n!info commands --> Commands List');
-//             }
-//             break;
-//         case 'emote':
-//             if(args[1] === 'sad'){
-//                 if(!msg.member.roles.find(r => r.name === "Founding Fathers")) return msg.channel.send('u aint got permission bruv periodt.');
-//                 msg.channel.sendMessage(':( oof');
-//                 break;
-//             }
-//             else if(args[1] === 'pog'){
-//                 if(!msg.member.roles.find(r => r.name === "Founding Fathers")) return msg.channel.send('u aint got permission bruv periodt.');
-//                 msg.channel.sendMessage('sorry fam i aint finished yet ¯\_(ツ)_/¯');
-//                 break;
-//             }
-//             else if(args[1] === 'lmao'){
-//                 if(!msg.member.roles.find(r => r.name === "Founding Fathers")) return msg.channel.send('u aint got permission bruv periodt.');
-//                 msg.channel.sendMessage('lemo');
-//                 break;
-//             }
-//             else if(args[1] === 'frick'){
-//                 if(!msg.member.roles.find(r => r.name === "Founding Fathers")) return msg.channel.send('u aint got permission bruv periodt.');
-//                 msg.channel.sendMessage('error frikin not done yet');
-//                 break;
-//             }
-//             else if(args[1] === 'YES'){
-//                 if(!msg.member.roles.find(r => r.name === "Founding Fathers")) return msg.channel.send('u aint got permission bruv periodt.');
-//                 msg.channel.sendMessage('this machine broke');
-//                 break;
-//             }
-//             else{
-//                 msg.channel.sendMessage('bro dis aint a command wtf');
-//                 break;
-//             }  
-//     }
+    if(!command.startsWith(PREFIX)) return;
 
-// })
+    let cmd = bot.commands.get(command.slice(PREFIX.length));
+    if(cmd) cmd.run(bot, message, args);
+
+
+});
 
 bot.login(botSettings.token);
---
+++ commands/botinfo.js
@@ -0,0 +1,7 @@
+module.exports.run = async(bot, message, args) =>{
+    console.log("Botinfo Works!");
+}    
+
+module.exports.help = {
+    name: "botinfo"
+}
